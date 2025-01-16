da8c8d927eeb889ddbf59a3ca2d9762eb12f55c2
Ilanattal <i29attal.attal@gmail.com>
2025-01-16 19:50:33 +0200

    Deploy updated build with fixed base path

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Board from "./components/Board";
-const App = () => {
-    return (_jsxs("div", { className: "app", children: [_jsx("h1", { children: "Tetris en React" }), _jsx(Board, {})] }));
-};
-export default App;
